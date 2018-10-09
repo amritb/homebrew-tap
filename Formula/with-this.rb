6029dc04edfd3de2444a5f02166a26dab2ffa39c
Amrit Bera <amritbera@gmail.com>
2018-10-09 23:07:13 +0200

    bump version

--- Formula/with-this.rb
+++ Formula/with-this.rb
@@ -1,8 +1,8 @@
 class WithThis < Formula
     desc "With let's you run any shell command with variables, in parallel."
     homepage "https://github.com/amritb/with-this"
-    url "https://github.com/amritb/with-this/archive/v0.1-alpha.0.tar.gz"
-    sha256 "ac8976a592e6832b8f0ad908bee27f1ac9870efad759681df3185050cda5c648"
+    url "https://github.com/amritb/with-this/archive/v0.1-beta.0.tar.gz"
+    sha256 "f4cb0802ba1066ac0a19d13b2cd25d829b1f36179196b70a4e32d7317b607cec"
     depends_on "go" => :build
   
     def install
@@ -15,7 +15,7 @@ class WithThis < Formula
     end
   
     test do
-      assert_match "with version v0.1-alpha.0", shell_output("#{bin}/with --version 2>&1", 2)
+      assert_match "with version v0.1-beta.0", shell_output("#{bin}/with --version 2>&1", 2)
     end
   end
   
